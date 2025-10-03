f8aa2a7a1b9ec44a3c741f59d2e04cedf22692f1
Manish karki <educationmaterial44@gmail.com>
2025-10-03 22:37:58 +0545

    set up Neon Postgres

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello from manish ");
+app.get('/', (req, res) => {
+  res.status(200).send('hello from manish ');
 });
 
 export default app;
