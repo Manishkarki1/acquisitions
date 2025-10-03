73460a2ee075b898b3558e067e1991ea2129a64c
Manish karki <educationmaterial44@gmail.com>
2025-10-03 23:42:29 +0545

    setting utils

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+// import logger from '#config/logger.js';
+
+// export const signup = async (req, res, next) => {
+//   try {
+
+//   } catch (e) {
+//     logger.error('failed to sign up', e);
+
+//   }
+// };
